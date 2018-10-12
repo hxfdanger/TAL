3db6b3db5fa01bd759d4f0d6c3d7c682e9c0fc19
hxfdanger <houssem-farhat@live.fr>
2018-10-12 17:28:05 +0200

    commit arbre

---
+++ arbre.py
@@ -0,0 +1,51 @@
+class tree:
+
+    def __init__(self):
+        """
+        constructor: tree
+        input: sentence list[string]
+        """
+        self.vertices = []
+
+    def push(vertex):
+        """
+        push vertex
+        """
+        self.vertices.append(vertex)
+
+    def pop(i):
+        """
+        show and delete the i vertex in vertices
+        """
+        self.vertices.pop(i)
+
+
+class Vertex:
+
+    def __init__(self, word, index, parent, nodes):
+        """
+        constructor: Vertex
+        input:
+            word=string
+            index=int
+            parent=Vertex
+            nodes= list[]
+        """
+
+        self.word = word
+        self.index = index
+        self.parent = parent
+        self.nodes = nodes
+
+
+class laison:
+
+    def __init__(self, index, labels):
+        """
+
+        """
+
+        self.word = word
+        self.index = index
+        self.parent = parent
+        self.nodes = []
