9cc4ee87f44c353d3ccb1460b25689a9e07bb7cc
hxfdanger <houssem-farhat@live.fr>
2018-10-16 13:26:59 +0200

    modification arbre.py ("POO")

--- arbre.py
+++ arbre.py
@@ -1,130 +1,155 @@
 class Tree:
 
-	def __init__(self):
-		"""
-		constructor: Tree
-		input: sentence list[string]
-		"""
-		self.vertices = []
-		
-	def __init__(self,sentence=[]):
-		"""
-		constructor: Tree
-		input: sentence list[string]
-		Création d'un arbre contenant un vertex par mots de la sentence
-		Aucune liaison n'est crée
-		"""
-		self.vertices = []
-		# Ajout de la racine
-		vertex = Vertex("Root",0,None,list())
-		self.vertices.append(vertex)
-		
-		i = 1 # Index dans la phrase
-		for word in sentence:
-			# Ajout des mots de la phrase dans l'arbre
-			vertex = Vertex(word,i,None,list())
-			self.vertices.append(vertex)
-			i+=1
-		
-	def push(vertex):
-		"""
-		push vertex to the end of the tree
-		"""
-		return self.vertices.append(vertex)
+    def __init__(self):
+        """
+        constructor: Tree
+        input: sentence list[string]
+        """
+        self.vertices = []
 
-	def pop():
-		"""
-		return the first vertex in the tree
-		"""
-		return self.vertices.pop(0)
-		
-	def print_tree(self):
-		"""
-		Affiche l'arbre
-		"""
-		for vertex in self.vertices:
-			print("Indice ",vertex.index," Word ",vertex.word)
-			if vertex.parent is not None:
-				print("\tParent (",vertex.parent.index,") ",vertex.parent.word)
-			for liaison in vertex.nodes:
-				print("\tTarget ",liaison.target)
-		
-	def search(self,word):
-		"""
-		Retourne le vertex qui contient word si il existe
-		Sinon retourne None
-		"""
-		for vertex in self.vertices:
-			if vertex.word == word:
-				return vertex
-		return None
+    def fill(self, sentence=[]):
+        """
+        constructor: Tree
+        input: sentence list[string]
+        Création d'un arbre contenant un vertex par mots de la sentence
+        Aucune liaison n'est crée
+        """
+        self.vertices = []
+        # Ajout de la racine
+        vertex = Vertex("Root", 0, None, list())
+        self.push(vertex)
 
-	def link(self,word_i,labels,word_j):
-		"""
-		input:
-			wordi_i=string
-			labels=string[]
-			word_j=string
-		Crée une liaison de wi vers wj avec labels
-		"""
-		vertex_i = self.tree.search(word_i)
-		vertex_j = self.tree.search(word_j)
-		if vertex_i is not None and vertex_j is not None:
-			liaison = Liaison(index=vertex_j, labels=labels)
-			vertex_i.nodes.append(liaison)
-			vertex_j.parent = vertex_i
- 
+        i = 1  # Index dans la phrase
+        for word in sentence:
+            # Ajout des mots de la phrase dans l'arbre
+            vertex = Vertex(word, i, None, list())
+            self.push(vertex)
+            i += 1
+
+    def push(self, vertex):
+        """
+        push vertex to the end of the tree
+        """
+        return self.vertices.append(vertex)
+
+    def pop():
+        """
+        return the first vertex in the tree
+        """
+        return self.vertices.pop(0)
+
+    def print_tree(self):
+        """
+        Affiche l'arbre
+        """
+        for vertex in self.vertices:
+            vertex.show_vertex()
+
+    def search(self, word):
+        """
+        Retourne le vertex qui contient word si il existe
+        Sinon retourne None
+        """
+        for vertex in self.vertices:
+            if vertex.get_word() == word:
+                return vertex
+        return None
+
+    def link(self, word_i, labels, word_j):
+        """
+        input:
+                wordi_i=string
+                labels=string[]
+                word_j=string
+        Crée une liaison devertex.show_liaison() wi vers wj avec labels
+        """
+        vertex_i = self.tree.search(word_i)
+        vertex_j = self.tree.search(word_j)
+        if vertex_i is not None and vertex_j is not None:
+            liaison = Liaison(index=vertex_j, labels=labels)
+            vertex_i.nodes.append(liaison)
+            vertex_j.parent = vertex_i
+
+
 class Vertex:
 
-	def __init__(self, word, index, parent, nodes=[]):
-		"""
-		constructor: Vertex
-		input:
-			word=string
-			index=int
-			parent=Vertex
-			nodes= Liaisons list[]
-		"""
+    def __init__(self, word, index, parent, nodes=[]):
+        """
+        constructor: Vertex
+        input:
+                word=string
+                index=int
+                parent=Vertex
+                nodes= Liaisons list[]
+        """
 
-		self.word = word
-		self.index = index
-		self.parent = parent
-		self.nodes = nodes
+        self.word = word
+        self.index = index
+        self.parent = parent
+        self.nodes = nodes
+		def get_word():
+			return self.word
+
+		def get_index():
+			return self.index
+
+        def show_liaison():
+            for node in self.nodes:
+                print("\tTarget ", node.get_target())
+				print("\tType Laison : ", node.get_labels())
+
+        def show_parent():
+			if self.parent is not None:
+                print("\tParent (", self.parent.get_index(), ") ", vertex.parent.get_word())
+
+		def show_vertex():
+			print("Indice ", self.get_index(), " Word ", self.get_word())
+			self.show_parent()
+			self.show_liaison()
+
 
 
 class Liaison:
 
-	def __init__(self, target, labels=[]):
-		"""
-		input :
-			target= Vertex
-			labels= String[]
-		"""
+    def __init__(self, target, labels=[]):
+        """
+        input :
+                target= Vertex
+                labels= String[]
+        """
 
-		self.target = target
-		self.labels = labels
+        self.target = target
+        self.labels = labels
+
+        def get_target():
+            return self.target
+
+        def get_labels():
+            return self.labels
+
+
 
 
 class Pile:
 
-	def __init__(self):
-		"""
-		"""
-		self.pile = []
+    def __init__(self):
+        """
+        """
+        self.pile = []
 
-	def push(self,word):
-		"""
+    def push(self, word):
+        """
 
-		"""
-		return self.pile.append(word)
+        """
+        return self.pile.append(word)
 
-	def pop(self):
-		"""
+    def pop(self):
+        """
 
-		"""
-		if len(self.pile) > 0:
-			return self.pile.pop(len(self.pile) - 1)
-		return None
-		
-	def len(self):
-		return len(self.pile)
+        """
+        if len(self.pile) > 0:
+            return self.pile.pop(len(self.pile) - 1)
+        return None
+
+    def len(self):
+        return len(self.pile)
