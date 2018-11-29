eb244f789bd72f0ffa02a39cc3f6794b3ea78867
hxfdanger <houssem-farhat@live.fr>
2018-11-29 16:14:28 +0100

    add files

--- Features.py
+++ Features.py
@@ -3,143 +3,162 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 
+
 class Features:
-	"""
-	Objectif : 
-		Construire la liste des features présent dans un fichiers fm
-		Et constitué le dataset corespndant au features
-	"""
-	
-	def __init__(self, features_file):
-		"""
-		constructor: Features
-		input:
-				features_file = fichier contenant la liste des features
-		Remplie la liste des features en lisant le fichier features_file
-		"""
-		self.names = list()
-		
-		# Lecture du fichier
-		with open(features_file,'r') as f:
-			lines = f.readlines()
-		# Remplissage des noms 
-		for line in lines:
-			line = line.split(".")
-			line[-1] = line[-1].rstrip('\n')
-			
-			if line[0] == 'S':
-				idx = int(line[1])
-				feat = line[2]
-				self.names.append(("Pile",idx,feat))
-			elif line[0] == 'B':
-				idx = int(line[1])
-				feat = line[2]
-				self.names.append(("Buffer",idx,feat))
-			else:
-				self.names.append(line[0])
-		print("Features prise en compte : ",self.names)
-		
-		# Dataset
-		self.datas = list()
-		self.labels = list()
-		
-		# OneHot encoders pour crée les vecteurs one hot
-		self.oneHot_encoders = list()
-		for features in self.names:
-			self.oneHot_encoders.append(OneHotEncoder(sparse=False,handle_unknown='ignore'))
-		
-	def extract_features(self, pile, buff, tree):
-		"""
-		input:
-				pile = La pile courante
-				buff = Le buffer courant
-				tree = L'arbre courant
-		Extrait les features de l'état courant (pile,buff,tree) et les 
-		ajoute à self.datas
-		"""
-		data = list() # Liste des features de l'états courant
-		
-		for feature in self.names: # Pour toutes les features connues
-			print(feature)
-			if feature[0] == 'Pile': # Si la feature concerne la pile
-				idx = feature[1]
-				feat = feature[2]
-				idx_pile = pile.see(idx)
-				if idx_pile != None:
-					data.append(tree.vertices[idx_pile].get_elementWord(element=feat))
-					#print(data)
-				else:
-					data.append('NA') # Donnée non aquise
-										
-			elif feature[0] == 'Buffer': # Si la feature concerne le buffer
-				idx_buff = feature[1]
-				feat = feature[2]
-				
-				idx = buff.see(0)
-				if idx == None: # Si le buffer est vide on ce place juste après le dernier mot
-					idx = len(tree.vertices)
-					
-				if idx+idx_buff < 0 or idx+idx_buff >= len(tree.vertices): # Si la features concerne un élément hors de la phrase
-					data.append('NA') # Donnée non aquise
-				else:
-					data.append(tree.vertices[idx+idx_buff].get_elementWord(element=feat))
-					
-			elif feature == 'DIST': # Si la feature est une distance
-				Spile = pile.see(0)
-				Sbuff = buff.see(0)
-				if Spile != None and Sbuff != None:
-					data.append(abs(tree.vertices[Spile].get_index() - tree.vertices[Sbuff].get_index()))
-				else:
-					data.append('NA') # Donnée non aquise
-			
-		self.datas.append(data)
-		
-		#print(data)
-		return data
-		
-	def convert_data_to_one_hot(self,data):
-		"""
-		Converti la liste de features data en un tableau 1D de vecteur one hot 
-		"""
-		if len(data) != len(self.names):
-			print("La donnée ne posséde pas autant de features que demander !")
-			return None
-		
-		new_data = list()
-		for i,feature in enumerate(data):
-			tmp = np.array(feature).reshape(1, -1)
-			tmp = self.oneHot_encoders[i].transform(tmp)
-			new_data.append(tmp.reshape(-1))
-			
-		new_data = np.asarray(new_data)
-		new_data = new_data.flatten()
-		print(new_data)	
-		return new_data
-		
-	def convert_datas_to_one_hot(self):
-		"""
-		Entrainent les labels encoders sur le dataset
-		Converti les self.datas en une liste de liste de vecteur one hot
-		"""
-		if len(self.datas) <= 0:
-			print("Le dataset est vide, il est impossible d'entrainer les labels encoders!")
-			return None
-			
-		# Entrainment des oneHot_encoders
-		values = np.array(self.datas)
-		#values = values.reshape(np.shape(values)[0],np.shape(values)[1], 1)
-		for i,encoder in enumerate(self.oneHot_encoders):
-			#print(values[:,i])
-			encoder = encoder.fit(values[:,i].reshape(-1, 1))
-		
-		# Convertion du dataset
-		for data in self.datas:
-			data = self.convert_data_to_one_hot(data)
-		
-		return self.datas
-		
+    """
+    Objectif :
+            Construire la liste des features présent dans un fichiers fm
+            Et constitué le dataset corespndant au features
+    """
+
+    def __init__(self, features_file):
+        """
+        constructor: Features
+        input:
+                        features_file = fichier contenant la liste des features
+        Remplie la liste des features en lisant le fichier features_file
+        """
+        self.names = list()
+
+        # Lecture du fichier
+        with open(features_file, 'r') as f:
+            lines = f.readlines()
+        # Remplissage des noms
+        for line in lines:
+            line = line.split(".")
+            line[-1] = line[-1].rstrip('\n')
+
+            if line[0] == 'S':
+                idx = int(line[1])
+                feat = line[2]
+                self.names.append(("Pile", idx, feat))
+            elif line[0] == 'B':
+                idx = int(line[1])
+                feat = line[2]
+                self.names.append(("Buffer", idx, feat))
+            else:
+                self.names.append(line[0])
+        print("Features prise en compte : ", self.names)
+
+        # Dataset
+        self.datas = list()
+        self.labels = list()
+
+        # OneHot encoders pour crée les vecteurs one hot
+        self.oneHot_encoders = list()
+        for features in self.names:
+            self.oneHot_encoders.append(OneHotEncoder(
+                sparse=False, handle_unknown='ignore'))
+
+    def extract_features(self, pile, buff, tree):
+        """
+        input:
+                        pile = La pile courante
+                        buff = Le buffer courant
+                        tree = L'arbre courant
+        Extrait les features de l'état courant (pile,buff,tree) et les
+        ajoute à self.datas
+        """
+        data = list()  # Liste des features de l'états courant
+
+        for feature in self.names:  #  Pour toutes les features connues
+            print(feature)
+            if feature[0] == 'Pile':  # Si la feature concerne la pile
+                idx = feature[1]
+                feat = feature[2]
+                idx_pile = pile.see(idx)
+                if idx_pile != None:
+                    data.append(
+                        tree.vertices[idx_pile].get_elementWord(element=feat))
+                    # print(data)
+                else:
+                    data.append('NA')  # Donnée non aquise
+
+            elif feature[0] == 'Buffer':  # Si la feature concerne le buffer
+                idx_buff = feature[1]
+                feat = feature[2]
+
+                idx = buff.see(0)
+                if idx == None:  # Si le buffer est vide on ce place juste après le dernier mot
+                    idx = len(tree.vertices)
+
+                # Si la features concerne un élément hors de la phrase
+                if idx + idx_buff < 0 or idx + idx_buff >= len(tree.vertices):
+                    data.append('NA')  # Donnée non aquise
+                else:
+                    data.append(
+                        tree.vertices[idx + idx_buff].get_elementWord(element=feat))
+
+            elif feature == 'DIST':  # Si la feature est une distance
+                Spile = pile.see(0)
+                Sbuff = buff.see(0)
+                if Spile != None and Sbuff != None:
+                    data.append(
+                        abs(tree.vertices[Spile].get_index() - tree.vertices[Sbuff].get_index()))
+                else:
+                    data.append('NA')  # Donnée non aquise
+
+        self.datas.append(data)
+
+        # print(data)
+        return data
+
+    def convert_data_to_one_hot(self, data):
+        """
+        Converti la liste de features data en un tableau 1D de vecteur one hot
+        """
+        if len(data) != len(self.names):
+            print("La donnée ne posséde pas autant de features que demander !")
+            return None
+
+        new_data = list()
+        for i, feature in enumerate(data):
+            tmp = np.array(feature).reshape(1, -1)
+            tmp = self.oneHot_encoders[i].transform(tmp)
+            new_data.append(tmp.reshape(-1))
+
+        new_data = np.asarray(new_data)
+
+        new_data = new_data.flatten()
+        # print(new_data)
+        return new_data.copy()
+
+    def convert_datas_to_one_hot(self):
+        """
+        Entrainent les labels encoders sur le dataset
+        Converti les self.datas en une liste de liste de vecteur one hot
+        """
+        if len(self.datas) <= 0:
+            print(
+                "Le dataset est vide, il est impossible d'entrainer les labels encoders!")
+            return None
+
+        # Entrainment des oneHot_encoders
+        values = np.array(self.datas)
+        # print(values)
+        #values = values.reshape(np.shape(values)[0],np.shape(values)[1], 1)
+        for i, encoder in enumerate(self.oneHot_encoders):
+            # print(values[:,i])
+            encoder = encoder.fit(values[:, i].reshape(-1, 1))
+
+        X_data = []
+        # Convertion du dataset
+        for data in self.datas:
+
+            X = self.convert_data_to_one_hot(data)
+            X0 = X[0]
+            X1 = X[1]
+            X2 = X[2]
+            X = np.concatenate((X0, X1, X2), axis=0)
+            # print(X)
+            X_data.append(X)
+
+        X_data = np.asarray(X_data)
+        return X_data
+
 # Pour faire des One-hot
 # https://machinelearningmastery.com/how-to-one-hot-encode-sequence-data-in-python/
 
+
 if(__name__ == "__main__"):
-	features = Features("f3_tbp.fm")
+    features = Features("f3_tbp.fm")
--- neural_network.py
+++ neural_network.py
@@ -1,25 +1,79 @@
 # neurone NW
 # entré
 #
-
+from keras.utils.np_utils import to_categorical
+from sklearn import preprocessing
+import numpy as np
+from ConstructAllTree import ConstructAllTree
+from Features import Features
+from Oracle import Oracle
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+from numpy import argmax
+
 import keras
 
-model = Sequential()
-model.add(Dense(32, activation='relu', input_dim=100))
-model.add(Dense(10, activation='softmax'))
-model.compile(optimizer='rmsprop',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
 
-# Generate dummy data
-import numpy as np
-data = np.random.random((1000, 100))
-labels = np.random.randint(10, size=(1000, 1))
+# Test de la classe Oracle et Features
+mcd = (('INDEX', 'INT'), ('FORM', 'INT'), ('LEMMA', 'INT'), ('POS', 'SYM'), ('X1', 'INT'),
+       ('MORPHO', 'INT'), ('GOV', 'SYM'), ('LABEL', 'SYM'), ('X2', 'SYM'), ('X3', 'SYM'))
 
-# Convert labels to categorical one-hot encoding
-one_hot_labels = keras.utils.to_categorical(labels, num_classes=10)
+# Lecture du fichier conllu
+obj_generateAlltree = ConstructAllTree(
+    "test_conllu.txt", mcd, False)
+all_tree = obj_generateAlltree.get_allTree()
 
+features = Features("f1_tbp.fm")
+
+for tree in all_tree:
+    # tree.print_tree()
+
+    A = Oracle(tree, features)
+    result_tree = A.run()
+    # result_tree.print_tree()
+"""print("Liste des données (X) :")
+print(features.datas[1])
+print("Liste des labels (Y) :")
+print(features.labels[1])
+print("Nb label : ", len(features.labels))
+print("Nb data : ", len(features.datas))"""
+
+X_train = features.convert_datas_to_one_hot()
+print(X_train[1])
+# https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.LabelEncoder.html
+# to use LabelEncoder
+"""Y = np.asarray(features.labels)
+le = preprocessing.LabelEncoder()
+print("Y_train", Y)
+le.fit(Y)
+print(le.classes_)
+nb_class = len(le.classes_)
+print(nb_class)
+Y1 = le.transform(Y)
+print(Y1)
+# use one encoder
+Y_train = to_categorical(Y1, nb_class)
+print(Y_train)
+# reverse to_categorical
+Y2 = argmax(Y_train, axis=1)
+print(Y2)
+# reverse LabelEncoder
+Y3 = le.inverse_transform(Y2)
+print(Y3)
+
+
+def create_neural_network_model(num_classes, input_dim):
+    model = Sequential()
+    model.add(Dense(128, activation='relu', input_dim=input_dim))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
+
+
+input_dim = X_train.shape[1]
+model1 = create_neural_network_model(nb_class, input_dim)
 # Train the model, iterating on the data in batches of 32 samples
-model.fit(data, one_hot_labels, epochs=10, batch_size=32)
+model1.fit(X_train, Y_train, epochs=1000)"""
