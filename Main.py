1f13f66761c8ab7598982933eefa7f7a4fff25ff
hxfdanger <houssem-farhat@live.fr>
2018-11-30 01:24:04 +0100

    correc some bugs and testbut nothing ki marche

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ import numpy as np
 from numpy import argmax
 from ConstructAllTree import *
 from Features import *
+from neural_network import *
 
 from Oracle import *
 
@@ -28,7 +29,8 @@ def get_xy(file_conllu, file_features):
     mcd = get_mcd()
     obj_generateAlltree = ConstructAllTree(file_conllu, mcd, True)
 
-    all_tree = obj_generateAlltree.get_allTreeProjectiviser()
+    all_tree = obj_generateAlltree.get_allTreeProjectiviser()[:2]
+    print(len(all_tree))
     # print(all_tree)
     #
     features = Features(file_features)
@@ -52,6 +54,7 @@ def get_data(file_features, file_train_conllu, file_test_conllu):
     # Lecture du fichier conllu
 
     x_train, y_train = get_xy(file_train_conllu, file_features)
+    print("first file ok")
     x_test, y_test = get_xy(file_test_conllu, file_features)
 
     return x_train, x_test, y_train, y_test
@@ -62,7 +65,18 @@ if(__name__ == "__main__"):
     x_train, x_test, y_train, y_test = get_data(
         "f1_tbp.fm", "UD_French-GSD/fr_gsd-ud-train.conllu", "UD_French-GSD/fr_gsd-ud-test.conllu")
     # x_train,x_test,y_train,y_test = get_data("Data/f1_tbp.fm","test.txt","test.txt")
-    print("X_train=", x_train.shape)
+    print("x_train=", x_train.shape)
     print("X_test=", x_test.shape)
     print("Y_train=", y_train.shape)
     print("Y_test=", y_test.shape)
+
+    input_dim = x_train.shape[1]
+    print("input_dim= ", input_dim)
+    nb_class = y_train.shape[1]
+    print("nb_class= ", nb_class)
+    model1 = create_neural_network_model(nb_class, input_dim)
+    # Train the model, iterating on the data in batches of 32 samples
+    model1.fit(x_train, y_train, epochs=1000)
+    score = model1.evaluate(x_test, y_test)
+    print("%s: %.2f%%" % (model1.metrics_names[1], score[1] * 100))
+    print("loss %f.2" % score[0])
