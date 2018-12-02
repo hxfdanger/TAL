1e33fd14ccbec3d7811f2726e18c631a1fa5e9f0
hxfdanger <houssem-farhat@live.fr>
2018-12-02 02:46:37 +0100

    changes util and features to faster load vectors

--- Main.py
+++ Main.py
@@ -67,21 +67,20 @@ if(__name__ == "__main__"):
     features_file = "Data/f2_tbp.fm"
     #conllu_file = "Data/fr_gsd-ud-train.conllu"
     conllu_file = "Data/fr_gsd-ud-train.conllu"
-    weight_embedding_file = "Data/embd_file_vectors/embd.vec"
+    weight_embedding_file = "Data/embd.vec"
     x_train, y_train = get_data(
         features_file, conllu_file, weight_embedding_file)
     # x_train,x_test,y_train,y_test = get_data("Data/f1_tbp.fm","test.txt","test.txt")
     print("x_train=", x_train.shape)
     print("Y_train=", y_train.shape)
     print("start_train")
-    input_dim = x_train.shape[1]
+    """input_dim = x_train.shape[1]
     print("input_dim= ", input_dim)
     nb_class = y_train.shape[1]
     print("nb_class= ", nb_class)
-    model1 = create_neural_network_model(nb_class, input_dim)
+    model = create_neural_network_model(nb_class, input_dim)
     # Train the model, iterating on the data in batches of 32 samples
-    model1.fit(x_train, y_train, epochs=1000)
-    """score = model1.evaluate(x_test, y_test)
-    print("%s: %.2f%%" % (model1.metrics_names[1], score[1] * 100))
+    model.fit(x_train, y_train, epochs=1000)
 
-    print("loss %f.2" % score[0])"""
+    model.save('model_f2.h5')  # creates a HDF5 file 'my_model.h5'
+    del model  # deletes the existing model"""
